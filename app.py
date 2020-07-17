d2fa1fd7d421da4d6c9a088ee9aec917dd4078fd
Kaustubh Milindrao Sardar <kaustubhsardar@gmail.com>
2020-07-16 18:12:24 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,58 @@
+import pickle
+import numpy as np
+from flask import Flask, request
+import os
+
+model = None
+app = Flask(__name__)
+UPLOAD_FOLDER = './upload'
+
+def load_model():
+    global model
+    
+    with open('iris_trained_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+
+@app.route('/')
+def home_endpoint():
+    return 'Hello World!'
+
+
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    # Works only for a single sample
+    if request.method == 'POST':
+        data = request.get_json()  # Get data posted as a json
+        data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
+        prediction = model.predict(data)  # runs globally loaded model on the data
+    return str(prediction[0])
+
+
+
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+        file1.save(path)
+        return path
+
+        return 'ok'
+    return '''
+    <h1>Upload new File</h1>
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" name="file1">
+      <input type="submit">
+    </form>
+    '''
+
+if __name__ == '__main__':
+    load_model()  # load model at the beginning once only
+    app.run(host='0.0.0.0', port=80)
